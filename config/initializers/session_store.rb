79314efefd7c63b5523fb5ce023857c5435f60c1
computer science <csuser@localhost.localdomain>
2014-03-19 17:08:00 -0700

    First commit.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+COSC617Project::Application.config.session_store :cookie_store, key: '_COSC617Project_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# COSC617Project::Application.config.session_store :active_record_store
